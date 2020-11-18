adfef44cb9715876e0402bb71b17ab1c949aa061
vovchyk bratyk <40668075+vovchykbratyk@users.noreply.github.com>
2020-11-18 12:04:25 -0500

    Update raster.py

--- raster.py
+++ raster.py
@@ -30,31 +30,61 @@ class RasterQ:
             dt = 'NITF'
         else:
             dt = 'Raster'
+            
+        try:
+            with rasterio.open(self.raster_file) as r:
+                try:
+                    epsg = r.crs.to_epsg()
+                    bounds = r.bounds
+                    if epsg:
+                        if epsg != 4326:
+                            bounds = bounds.left, bounds.bottom, bounds.right, bounds.top
+                            minx, miny, maxx, maxy = static.to_wgs84(epsg, bounds)
+                        else:
+                            minx, miny, maxx, maxy = bounds.left, bounds.bottom, bounds.right, bounds.top
 
-        with rasterio.open(self.raster_file) as r:
-            try:
-                epsg = r.crs.to_epsg()
-                if epsg:
-                    if epsg != 4326:
-                        bounds = r.bounds.left, r.bounds.bottom, r.bounds.right, r.bounds.top
-                        minx, miny, maxx, maxy = static.to_wgs84(epsg, bounds)
-                    else:
-                        bounds = r.bounds
-                        minx, miny, maxx, maxy = bounds.left, bounds.bottom, bounds.right, bounds.top
+                        boundary = Polygon([
+                            [minx, miny],
+                            [maxx, miny],
+                            [maxx, maxy],
+                            [minx, maxy]
+                        ])
 
-                    boundary = Polygon([
-                        [minx, miny],
-                        [maxx, miny],
-                        [maxx, maxy],
-                        [minx, maxy]
-                    ])
-
-                    return static.get_geojson_record(
-                        geom=boundary,
-                        datatype=dt,
-                        fname=r.name,
-                        path=os.path.split(self.raster_file)[0],
-                        nativecrs=r.crs.to_epsg())
-
-            except AttributeError:
-                pass
+                        return static.get_geojson_record(geom=boundary,
+                                                         datatype=dt,
+                                                         fname=r.name,
+                                                         path=os.path.split(self.raster_file)[0],
+                                                         nativecrs=r.crs.to_epsg())
+                except Exception as e:
+                    try:
+                        filename = None
+                        
+                        ds = gdal.Open(self.raster_file)
+                        md = ds.GetMetadata()
+                        filename = md.get('NITF_FTITLE', os.path.split(self.raster_file)[1])
+                        for k, v in md.items():
+                            if k == 'NITF_IGEOLO':
+                                bounds_str = v
+                                orig_coords = static.dms_to_dd(bounds_str[30:45])
+                                b_coords = static.dms_to_dd(bounds_str[45:])
+                                c_coords = static.dms_to_dd(bounds_str[:15])
+                                d_coords = static.dms_to_dd(bounds_str[15:30])
+                                
+                                boundary = Polygon([
+                                    [orig_coords[1], orig_coords[0]],
+                                    [b_coords[1], b_coords[0]],
+                                    [c_coords[1], c_coords[0]],
+                                    [d_coords[1], d_coords[0]]
+                                ])
+                                
+                                return static.get_geojson_record(geom=boundary,
+                                                                 datatype=dt,
+                                                                 fname=filename,
+                                                                 path='file:///' + os.path.split(self.raster_file)[0],
+                                                                 nativecrs=4326)
+                                                                 
+                    except Exception as e:
+                        print(f'Unable to open {self.raster_file} even with GDAL, giving up...')
+                        return None
+        except exception as e:
+            return None
