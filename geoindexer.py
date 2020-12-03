ed492ea93dd479e7906f6fbb3d77f17f66d7cfb8
vovchykbratyk <eric@eaglefamily.io>
2020-12-03 16:31:20 -0500

    rebase get_extents()

--- geoindexer.py
+++ geoindexer.py
@@ -1,19 +1,26 @@
-from handlers import Container, Exif, Lidar, Raster, Shapefile
+from datetime import datetime
+import fiona
+from handlers import Container, Exif, Lidar, Log, Raster, Shapefile
 import json
 import os
 from pathlib import Path
-from pyproj import CRS
+import sys
+from tqdm import tqdm
 
 
 def no_values(row):
     return f"No files in list: {row}."
 
 
+def now():
+    return datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
+
 class GeoCrawler:
 
     def __init__(self, path, types):
         """
-        Default GeoCrawler instance.
+        GeoCrawler constructor
 
         :param path: The path to be crawled
         :type path: str
@@ -47,124 +54,152 @@ class GeoIndexer:
 
     def __init__(self, file_list):
         """
-        Constructor. Ingests uncategorized list of file objects and sorts them into broad categories for
-        discrete processing.
+        GeoIndexer constructor
         """
-
         self.file_list = file_list
-        self.categorized = {
-            'containers': [],
-            'jpg_files': [],
-            'lidar_files': [],
-            'rasters': [],
-            'shapefiles': []
-        }
-        self.default_sr = CRS.from_user_input(4326)
+        self.errors = []
+        self.failures = {'files': [],
+                         'layers': []}
 
-        try:
+    def get_extents(self, logging=None):
+
+        # Get total number of datasets to process, including geodatabase layers
+        if len(self.file_list) > 0:
+
+            to_process = 0
             for f in self.file_list:
-                f = f.strip()
-                ext = os.path.splitext(os.path.split(f)[1])[1][1:]
-                if ext in ['gdb', 'gpkg', 'kml', 'kmz', 'json', 'geojson']:
-                    self.categorized['containers'].append(f)
-                elif ext in ['jpg', 'jpeg']:
-                    self.categorized['jpg_files'].append(f)
-                elif ext in ['las', 'laz']:
-                    self.categorized['lidar_files'].append(f)
-                elif ext in ['tif', 'ntf', 'nitf', 'dt0', 'dt1', 'dt2', 'tiff']:
-                    self.categorized['rasters'].append(f)
-                elif ext == "shp":
-                    self.categorized['shapefiles'].append(f)
+                to_process += GeoIndexer.get_layer_num(self, f)
 
-            self.categorized = {key: value for (key, value) in self.categorized.items() if value is not None}
+            # Set up the output
+            points = []
+            polygons = []
+            extents = {'type': 'FeatureCollection',
+                       'features': []}
 
-        except Exception as e:
-            print(e)
-            raise e
+            # Set up the report
+            stats = {'container_layers': 0,
+                     'web_images': 0,
+                     'lidar_point_clouds': 0,
+                     'rasters': 0,
+                     'shapefiles': 0}
 
-    def get_extents(self):
+            # Main iterator
+            for f in tqdm(self.file_list, desc='GeoIndexer progress', total=len(self.file_list), dynamic_ncols=True):
+                fext = GeoIndexer.get_extension(f)
 
-        report = {'container_layers': 0,
-                  'web_images': 0,
-                  'lidar_pointclouds': 0,
-                  'rasters': 0,
-                  'shapefiles': 0}
+                if fext in ['gdb', 'gpkg', 'db', 'sqlite']:
+                    try:
+                        cf = Container(f).get_props()
+                        for feat in cf['feats']:
+                            if feat:
+                                polygons.append(json.loads(feat))
+                                stats['container_layers'] += 1
+                            else:
+                                self.errors.append(f'{now()} - Problem processing layer {feat} in {f}')
+                                self.failures['layers'].append(f'{feat} ({f})')
+                        if len(cf['errors']) > 0:
+                            self.errors.append([e for e in cf['errors']])
+                            self.failures['layers'].append([f for f in cf['failed_layers']])
+                    except Exception as e:
+                        self.errors.append(f'{now()} - {e} - [{f}]')
+                        self.failures['files'].append(f)
+                        pass
 
-        points = []
-        polygons = []
-        
-        extents = {'type': 'FeatureCollection',
-                   'features': []}
-        
-        try:
-            for cf in self.categorized['containers']:
-                container_feats = Container(cf).get_props()
-                for feat in container_feats:
-                    if feat:
-                        polygons.append(json.loads(feat))
-                        report['container_layers'] += 1
-        except KeyError as ke:
-            print(no_values(ke))
-            pass
-        
-        try:    
-            for jf in self.categorized['jpg_files']:
-                points.append(Exif(jf).get_props())
-                report['web_images'] += 1
-                
-        except KeyError as ke:
-            print(no_values(ke))
-            pass
-        
-        try:
-            for lf in self.categorized['lidar_files']:
-                feat = Lidar(lf).get_props()
-                if feat:
-                    polygons.append(json.loads(feat))
-                    report['lidar_pointclouds'] += 1
-                    
-        except KeyError as ke:
-            print(no_values(ke))
-            pass
-        
-        try:    
-            for rf in self.categorized['rasters']:
-                feat = Raster(rf).get_props()
-                if feat:
-                    polygons.append(json.loads(feat))
-                    report['rasters'] += 1
-                    
-        except KeyError as ke:
-            print(no_values(ke))
-            pass
-        
-        try:        
-            for sf in self.categorized['shapefiles']:
-                feat = Shapefile(sf).get_props()
-                if feat:
-                    polygons.append(json.loads(feat))
-                    report['shapefiles'] += 1
-                    
-        except KeyError as ke:
-            print(no_values(ke))
-            pass
-        
-        if len(polygons) > 0:
-            extents['features'] = polygons
-        if len(points) > 0:
-            extents['features'] = points
-            
-        return json.dumps(extents), report
+                elif fext in ['jpg', 'jpeg']:
+                    try:
+                        points.append(Exif(f).get_props())
+                        stats['web_images'] += 1
+                    except Exception as e:
+                        self.errors.append(f'{now()} - {e} - [{f}]')
+                        self.failures['files'].append(f)
+                        pass
 
+                elif fext in ['laz', 'las']:
+                    try:
+                        lf = Lidar(f).get_props()
+                        if lf:
+                            polygons.append(json.loads(lf))
+                            stats['lidar_point_clouds'] += 1
+                        else:
+                            self.errors.append(f'{now()} - Problem processing Lidar file {f}')
+                            self.failures['files'].append(f)
+                    except Exception as e:
+                        self.errors.append(f'{now()} - {e} - [{f}]')
+                        pass
 
-# Testing
-if __name__ == '__main__':
-    searchpath = ""
-    ftypes = ['gpkg', 'json', 'geojson',
-              'jpg', 'jpeg', 'las', 'laz', 'tif', 'tiff', 'ntf',
-              'nitf', 'dt0', 'dt1', 'dt2', 'shp']
-    search = GeoCrawler(searchpath, ftypes).get_file_list()
+                elif fext in ['tiff', 'tif', 'ntf', 'nitf', 'dt0', 'dt1', 'dt2']:
+                    try:
+                        feat = Raster(f).get_props()
+                        if feat:
+                            polygons.append(json.loads(feat))
+                            stats['rasters'] += 1
+                        else:
+                            self.errors.append(f'{now()} - Problem accessing Raster {f}')
+                            self.failures['files'].append(f)
+                    except Exception as e:
+                        self.errors.append(f'{now()} - {e} - [{f}]')
+                        self.failures['files'].append(f)
+                        pass
 
-    srtd = GeoIndexer(search)
+                elif fext == 'shp':
+                    try:
+                        feat = Shapefile(f).get_props()
+                        if feat:
+                            polygons.append(json.loads(feat))
+                            stats['shapefiles'] += 1
+                        else:
+                            self.errors.append(f'{now()} - Problem accessing Shapefile {f}')
+                            self.failures['files'].append(f)
+                    except Exception as e:
+                        self.errors.append(f'{now()} - {e} - [{f}]')
+                        self.failures['files'].append(f)
+                        pass
 
-    print(srtd.get_extents())
+            # Assemble the GeoJSON object
+            if len(polygons) > 0:
+                for poly in polygons:
+                    extents['features'].append(poly)
+            if len(points) > 0:
+                for point in points:
+                    extents['features'].append(points)
+
+            # Summary statistics
+            stats['total_processed'] = sum([val for key, val in stats.items()])
+            stats['total_datasets'] = to_process
+            stats['success_rate'] = round(
+                ((float(stats.get('total_processed', 0)) / float(stats.get('total_datasets', 0))) * 100), 2)
+
+            # Output log if true
+            if logging:
+                log = Log(self.errors)
+                logname = log.to_file(logging)
+                stats['logfile'] = f'file:///{str(os.path.join(logging, logname))}'.replace("\\", "/")
+
+            return extents, stats, self.failures
+
+        else:
+            sys.exit('No files found to process.')
+
+    def get_layer_num(self, filepath: str):
+        """
+        Get the number of layers within a container, if the file is a container and can be read by fiona.
+        Otherwise, return 0 (if the container cannot be read) or 1 (if the file is not a container).
+
+        :return: int
+        """
+        extension = GeoIndexer.get_extension(filepath)
+        if extension in ['gdb', 'gpkg', 'db', 'sqlite']:
+            try:
+                numlayers = len(fiona.listlayers(filepath))
+                return numlayers
+            except Exception as e:
+                self.errors.append(f'{now()} - {e} - [{filepath}]')
+                return 0
+        else:
+            return 1
+
+    @staticmethod
+    def get_extension(filepath: str):
+        if filepath:
+            return os.path.splitext(os.path.split(filepath)[1])[1][1:].lower()
+        return None
