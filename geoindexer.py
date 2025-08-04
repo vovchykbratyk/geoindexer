13993bf91203bb38a3485c68c2b3ceb87f082237
VovchykBratyk <40668075+vovchykbratyk@users.noreply.github.com>
2025-08-04 09:53:32 -0400

    licensing docstring

--- geoindexer.py
+++ geoindexer.py
@@ -1,12 +1,28 @@
 """
 geoindexer.py
 
-Original author: Eric Eagle
-Initial creation: 2020
+==Licensing and Copyright==
+Copyright (C) 2025 Eric Eagle
 
-This script indexes geospatial and location-aware datasets across local or networked
-storage, extracting spatial metadata for storage in GeoJSON or GeoPackage.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+==Purpose==
+This script indexes geospatial and location-aware datasets across local
+or networked storage, extracting spatial metadata for storage in GeoJSON
+or GeoPackage.
 """
+
 
 from datetime import datetime
 from pathlib import Path
